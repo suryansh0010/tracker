5f82dc988916dfa03f5ee3cf14983590d107427e
suryansh0010 <suryanshawasthi9@gmail.com>
2025-04-17 22:45:32 +0530

    updated

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -8,7 +8,7 @@ export const Profile = () => {
           <img src="/profileimage.png" alt="" />
         </div>
         <div className="w-3/4 flex flex-col justify-center">
-          <h3 className="text-xl">Ishant</h3>
+          <h3 className="text-xl">Harshit</h3>
           <p>Web Developer/ Freelancer</p>
         </div>
       </div>
